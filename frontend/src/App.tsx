fa35f3426b0c8756aff538013aa6b5ad2dae8f8b
Mendrit Morina <mendritmorina4@gmail.com>
2024-02-07 14:01:00 +0100

    Initial setup project frontend and tailwind configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  //h-screen = height: 100vh
+  //items-center = align-items: center (horizontal center)
+  //justify-center = align-items: center (vertical center)
+
+  return (
+    <div className="App flex h-screen items-center justify-center">
+      <h1 className='text-5xl'>Mern</h1>
+    </div>
+  );
+}
+
+export default App;
